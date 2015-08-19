46f378b690238805410bfc854d0b9ed4ff7ac6d4
Alex Smith <alex.smith@redhat.com>
2015-08-19 00:14:34 -0400

    Get simple spec tests working.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
-describe 'docker_systemd' do
 
+describe 'docker_systemd' do
   context 'with defaults for all parameters' do
+    it { should compile }
     it { should contain_class('docker_systemd') }
   end
 end
